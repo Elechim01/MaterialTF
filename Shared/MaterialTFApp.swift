3152876523423ca9a132c4aede1b09ad55642190
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-10 11:19:07 +0200

    Initial Commit

---
+++ Shared/MaterialTFApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaterialTFApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 10/09/21.
+//
+
+import SwiftUI
+
+@main
+struct MaterialTFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
